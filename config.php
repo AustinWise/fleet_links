cee6f59ce16033105462be6d71756bbcb5d2b6f5
unknown <AustinWise@.(none)>
2009-03-07 20:33:00 -0800

    Moved code into it's own folder.

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?php
 $cfg['eve']['useFakeIGB'] = 0;
+$cfg['db']['server'] = 'localhost';
+$cfg['db']['username'] = 'root';
+$cfg['db']['password'] = 'Turtle';
+$cfg['db']['database'] = 'fleetlinks';
 ?>
